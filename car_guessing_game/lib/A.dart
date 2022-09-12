39053b76a8ae0d65706acb32629458fd26dd0d12
Muhammet İkbal Çakır <100643237+MuhammetCakirr@users.noreply.github.com>
2022-09-12 21:36:51 +0300

    paketleri yükledim, splash screen

---
+++ car_guessing_game/lib/A.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class APage extends StatelessWidget {
+  const APage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("SAYFA",style: TextStyle(fontSize: 30),),),);
+  }
+}
