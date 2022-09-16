e612959788e77837a028a916e4d04cb7315345e2
Mustafa <100375168+musttoprak@users.noreply.github.com>
2022-09-17 00:00:04 +0300

    afd

---
+++ car_guessing_game/lib/models/quentions.dart
@@ -0,0 +1,16 @@
+import 'package:car_guessing_game/models/car.dart';
+
+class Questions {
+  final Car car;
+
+  Questions(this.car);
+  
+  String reply(){
+    String model = car.model.modelName;
+    for(var i=0;i<=model.length;i++){
+      
+    }
+    return  " ";
+  }
+
+}
